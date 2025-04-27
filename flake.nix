6f239f135cb4eb6ff49780bd07e821e98fa746da
Ben Orchard <thefirstmuffinman@gmail.com>
2025-04-27 17:34:19 +0100

    test on GHC 9.10 +-1; update Nix flake

--- flake.nix
+++ flake.nix
@@ -24,6 +24,10 @@
       perSystem = { self', pkgs, config, ... }: {
         packages.default  = self'.packages.ghc910-type-level-bytestrings;
         devShells.default = self'.devShells.ghc910;
+        haskellProjects.ghc912 = {
+          basePackages = pkgs.haskell.packages.ghc912;
+          devShell = defDevShell "ghc912";
+        };
         haskellProjects.ghc910 = {
           basePackages = pkgs.haskell.packages.ghc910;
           devShell = defDevShell "ghc910";
@@ -31,25 +35,6 @@
         haskellProjects.ghc98 = {
           basePackages = pkgs.haskell.packages.ghc98;
           devShell = defDevShell "ghc98";
-        };
-        haskellProjects.ghc96 = {
-          basePackages = pkgs.haskell.packages.ghc96;
-          devShell = defDevShell "ghc96";
-        };
-        haskellProjects.ghc94 = {
-          basePackages = pkgs.haskell.packages.ghc94;
-          devShell = {
-            mkShellArgs.name = "ghc94";
-            hoogle = false;
-            tools = _: {
-              # as of 2024-10-12 nixpkgs can't build cabal-install on GHC 9.4,
-              # so use global package
-              cabal-install = pkgs.cabal-install;
-              haskell-language-server = null;
-              hlint = null;
-              ghcid = null;
-            };
-          };
         };
       };
     };
